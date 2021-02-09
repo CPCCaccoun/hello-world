1cc2ac26c87a66aeb10702adae5bbbf501e5cffa
Ramone <rthomp52@email.cpcc.edu>
2021-02-09 18:17:03 -0500

    Print Comment
    A line of code was added that prints some text.

--- Name.java
+++ Name.java
@@ -4,5 +4,6 @@ public class Name
 public static void main(String[] args)
 {
 System.out.println("My name is Ramone Thompson, nice to meet you!");
+System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
 }
 }
